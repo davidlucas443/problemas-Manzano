309a416b9ec55743f26dbbb148253942719b6e13
davidlucas443 <davidga19391@gmail.com>
2025-02-18 08:47:18 -0300

    Update questaoC.java

--- questaoC.java
+++ questaoC.java
@@ -8,13 +8,15 @@ public class questaoC {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner (System.in);
 		Double altura, raio, volume;
+		
 		System.out.println("digite o valor do raio");
 		raio = sc.nextDouble();
+		
 		System.out.println("digite o valor da altura");
 		altura = sc.nextDouble();
+		
 		volume = (3.14159*(raio*raio)*altura);
+		
 		System.out.println("o volume é igual a :" + volume);
-
 	}
-
 }
