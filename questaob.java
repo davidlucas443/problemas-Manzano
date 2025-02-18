b3422beb6fdfdbd0c61b712ff0e36473b0dfdc89
davidlucas443 <davidga19391@gmail.com>
2025-02-18 08:50:02 -0300

    Update questaob.java

--- questaob.java
+++ questaob.java
@@ -6,11 +6,14 @@ public class questaob {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
+		
 		Double c, f;
+		
 		System.out.println("digite o número em farenheit");
 		f = sc.nextDouble();
+		
 		c = ((f - 32) * 5) / 9;
+		
 		System.out.println("em celcius é igual a :" + c);
 	}
-
 }
