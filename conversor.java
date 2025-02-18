151026f313ce4ca2947e0ba13256e8f6cdab4499
davidlucas443 <davidga19391@gmail.com>
2025-02-18 08:46:54 -0300

    Update conversor.java

--- conversor.java
+++ conversor.java
@@ -6,11 +6,13 @@ public class conversor {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
+		
 		Double c, f;
 		System.out.println("digite o número em celcius");
 		c = sc.nextDouble();
+		
 		f = (c * 9 / 5 + 32);
+		
 		System.out.println("em farenheit é igual a :" + f);
 	}
-
 }
