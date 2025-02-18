e6a4384bfb1c7067e8eb067d3df03200724c9a15
davidlucas443 <davidga19391@gmail.com>
2025-02-18 08:46:12 -0300

    Update QuestaoH.java

--- QuestaoH.java
+++ QuestaoH.java
@@ -6,17 +6,22 @@ public class QuestaoH {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
+		
 		Double volume, comprimento, largura, altura;
+		
 		System.out.println("digite o valor do comprimento");
 		comprimento = sc.nextDouble();
+		
 		System.out.println("digite o valor da largura");
 		largura = sc.nextDouble();
+		
 		System.out.println("digite o valor da altura");
 		altura = sc.nextDouble();
+		
 		volume = comprimento*largura*altura;
+		
 		System.out.println("o valor do volume do retangulo é " + volume);
 				
-
 	}
 
 }
