89f7d6e3b24a0a2f57b40ef73a94ea119fc6317c
davidlucas443 <davidga19391@gmail.com>
2025-02-18 08:49:33 -0300

    Update questaoF.java

--- questaoF.java
+++ questaoF.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class questaoF {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner sc = new Scanner (System.in);
+
 		Double a, b, c, d;
+		
 		System.out.println("digite o valor de a");
 		a = sc.nextDouble();
+		
 		System.out.println("digite o valor de b");
 		b = sc.nextDouble();
+		
 		c = a;
 		d = b;
+		
 		System.out.println("o valor de A é" + d);
+		
 		System.out.println("o valor de B é " + c);
 				
 	}
-
 }
